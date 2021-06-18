b7fa3080dc749f12186ed3761d64ce5d70560997
DAlons <alonso.morey.arq@gmail.com>
2021-06-17 21:54:23 -0700

    Codigo c para salida de texto

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,7 +4,7 @@
  * main - print "Programming is like building a multilingual puzzle
  * return - sea usa cuando el retorno sea nulo
  */
-int main()
+int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
