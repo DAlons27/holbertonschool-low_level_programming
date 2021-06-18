8b5637f060c46e6d9df9c65d30dc19f4a595a1bf
DAlons <alonso.morey.arq@gmail.com>
2021-06-17 22:33:10 -0700

    Al fin salio, atento a los tabs para salida de linea de texto

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 
 /**
- * main - print "Programming is like building a multilingual puzzle
- * return - sea usa cuando el retorno sea nulo
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
