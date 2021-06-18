febe692bc35c14a7ca7364227ba67ac56dd67110
DAlons <alonso.morey.arq@gmail.com>
2021-06-17 21:32:29 -0700

    Codigo que permite imprimir linea de texto

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
 /**
- *main - funcion principal, punto de partida del programa
- *void - sea usa cuando el retorno sea nulo
+ * main - Salida de la frase "Programming is like building a multilingual puzzle
+ * return - sea usa cuando el retorno sea nulo
  */
 int main(void)
-{
-  puts ("\"Programming is like building a multilingual puzzle");
-  return (0);
-}
+  {
+    puts("\"Programming is like building a multilingual puzzle");
+    return(0);
+  }
