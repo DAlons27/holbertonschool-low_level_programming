f2ae87aaa7851bf4f2cfedeccd6c249a8cf42f2e
DAlons <alonso.morey.arq@gmail.com>
2021-06-17 21:37:11 -0700

    Codigo c para imprimir un texto

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
 /**
- * main - Salida de la frase "Programming is like building a multilingual puzzle
+ * main - print "Programming is like building a multilingual puzzle
  * return - sea usa cuando el retorno sea nulo
  */
 int main(void)
-  {
-    puts("\"Programming is like building a multilingual puzzle");
-    return(0);
-  }
+{
+puts ("\"Programming is like building a multilingual puzzle");
+return(0);
+}
