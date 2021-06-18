8a35dea56b80639fcd63744d19db229082ba74a3
DAlons <alonso.morey.arq@gmail.com>
2021-06-17 21:12:27 -0700

    Creando programa que imprima una linea de texto y una nueva linea al inal

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
-int main (void)
+
+/**
+ *main - funcion principal, punto de partida del programa
+ *void - sea usa cuando el retorno sea nulo
+ */
+int main(void)
 {
   puts ("\"Programming is like building a multilingual puzzle");
   return (0);
