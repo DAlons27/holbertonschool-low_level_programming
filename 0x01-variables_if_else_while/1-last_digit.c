c8ed32294c46c5396317488655bb64245fa96d4d
DAlons <alonso.morey.arq@gmail.com>
2021-06-20 14:04:37 -0700

    Ultimo digito usando solo if

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - n sera un numero al azar
+ *
+ * Return: si coincide dar cero
+ */
+
+int main(void)
+{
+int n;
+
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+
+printf("Last digit of %d is %d ", n, n % 10);
+if (n % 10 > 5)
+{
+printf("and is greater than 5\n");
+}
+
+if (n % 10 == 0)
+{
+printf("and is 0\n");
+}
+
+if (n % 10 < 6 && n % 10 != 0)
+{
+printf("and is less than 6 and not 0\n");
+}
+return (0);
+}
