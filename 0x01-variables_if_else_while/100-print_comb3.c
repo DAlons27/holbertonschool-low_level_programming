d1629029d21de6a55e6d8f217cfa2276486a9be7
DAlons <alonso.morey.arq@gmail.com>
2021-06-20 20:05:11 -0700

    Combinaciones con dos digitos

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - el programa dara las posibles combinaciones de dos digito
+ *
+ * Return: si coincide dara cero
+ */
+
+int main(void)
+{
+	int a, b;
+
+	for (a = 48; a <= 56; a++)
+	{
+		for (b = 49; b <= 57; b++)
+		{
+			if (b > a)
+			{
+				putchar(a);
+				putchar(b);
+				if (a != 56 || b != 57)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
