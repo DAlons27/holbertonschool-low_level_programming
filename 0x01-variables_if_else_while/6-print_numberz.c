378675b193388ef9b80df8d4698ed8e46c0b43ac
DAlons <alonso.morey.arq@gmail.com>
2021-06-20 18:29:12 -0700

    10 primeros numeros usando putchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - el programa dara los 10 primeros nuemros
+ *
+ * Return: si coincide dara cero
+ */
+
+int main(void)
+{
+int n;
+for (n = 48; n < 58; n++)
+{
+putchar(n);
+}
+putchar('\n');
+return (0);
+}
