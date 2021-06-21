c4322fbd3845b792211359dd4ffd7ccc04517088
DAlons <alonso.morey.arq@gmail.com>
2021-06-20 18:13:05 -0700

    Listando los 10 primeros numeros en base 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - el programa dara los 10 primeros nuemros
+ *
+ * Return: si coincide dara cero
+ */
+
+int main(void)
+{
+int n;
+for (n = 0; n < 10; n++)
+printf("%d", n);
+printf("\n");
+return (0);
+}
