5b7003e02c3545a0f3d3fe68fc9e80ab0066281d
DAlons <alonso.morey.arq@gmail.com>
2021-06-23 08:33:09 -0700

    Usando _putchar para salida de Holberton

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,19 @@
+#include<holberton.h>
+/**
+ * main - dar salida holberton.h
+ *
+ * Return: Dara como salida Holberton
+ */
+int main(void)
+{
+    _putchar('H');
+    _putchar('o');
+    _putchar('l');
+    _putchar('b');
+    _putchar('e');
+    _putchar('r');
+    _putchar('t');
+    _putchar('o');
+    _putchar('n');
+    return (0);
+}
