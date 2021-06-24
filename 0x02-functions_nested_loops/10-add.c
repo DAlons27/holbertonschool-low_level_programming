0e0ef00be89bc0014cd05c7a6573b348001cfd35
DAlons <alonso.morey.arq@gmail.com>
2021-06-23 21:46:35 -0700

    Suma de a + b usando add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * add - agrega dos valores y retornma resultado
+ * @a: primer entero
+ * @b: segundo entero
+ * Return: devuelve la suma de a y b
+ */
+
+int add(int a, int b)
+{
+	int c;
+
+	c = a + b;
+
+	return (c);
+}
