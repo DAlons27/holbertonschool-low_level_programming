e13b22d07b76a4396ad969d2b8635549245da1a2
DAlons <alonso.morey.arq@gmail.com>
2021-06-23 19:39:42 -0700

    _islower para buscar caracteres en miniscula

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,12 @@
+#include "holbeton.h"
+
+/**
+ * _islower - para buscar caracteres en minuscula
+ *
+ * Return: 1 si es caracter minuscula y 0 de lo cotrario
+ */
+
+int _islower(int c)
+{
+return (c >= 'a' && c <= 'z');
+}
