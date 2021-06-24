69ed3cac747056e1b23cdb0fbd0befb7ccf45f5e
DAlons <alonso.morey.arq@gmail.com>
2021-06-23 21:54:13 -0700

    Imprimira numeros naturales hasta el 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include "holberton.h"
+
+/**
+ * print_to_98 - imprimira los numeros naturales hasta el 98
+ * @n: aqui se almacena el numero que usaremos
+ */
+void print_to_98(int n)
+{
+	int i, j;
+
+	if (n <= 98)
+	{
+		for (i = n; i <= 98; i++)
+		{
+			if (i != 98)
+				printf("%d, ", i);
+			else if (i == 98)
+				printf("%d\n", i);
+		}
+	} else if (n >= 98)
+	{
+		for (j = n; j >= 98; j--)
+		{
+			if (j != 98)
+				printf("%d, ", j);
+			else if (j == 98)
+				printf("%d\n", j);
+		}
+	}
+}
