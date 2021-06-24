5b89a03e2e4c364201faa167b733d62d8e48b50e
DAlons <alonso.morey.arq@gmail.com>
2021-06-23 21:10:38 -0700

    Calculando el valor absoluto

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "holberton.hs"
+
+/**
+ * _abs - calcula el valor absoluto
+ * @n: el numero el cual servira para calcular su valor absoluto
+ * Return: devolucion del valor absoluto
+ */
+int _abs(int n)
+{
+if (n >= 0)
+{
+return (n);
+}
+return (-n);
+}
