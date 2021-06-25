4a8f159b2c77ecb660199616d1b937dabc6b621c
DAlons <alonso.morey.arq@gmail.com>
2021-06-25 00:42:48 -0700

    Multiplicando dos numeros

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * mul - Multiplicara dos numeros
+ * @a: 1er valor
+ * @b: 2do valor
+ * Return: devuelve el valor de la multiplicacion de a y b
+ */
+
+int mul(int a, int b)
+{
+int c;
+
+c = a * b;
+
+return (c);
+}
