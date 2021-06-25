bc7b8770a55f76d243f529eb0990e0baff7de0fc
DAlons <alonso.morey.arq@gmail.com>
2021-06-24 21:01:36 -0700

    Uso del isupper para buscar caracteres en mayuscula

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+
+/**
+ * _isupper - busca caracteres en mayuscula
+ * @c: el caracterer a verificar
+ * Return: 1 si es mayuscula, 0 de lo contrario
+ */
+
+int _isupper(int c);
+{
+return (c >= 'A' && c <= 'Z');
+}
