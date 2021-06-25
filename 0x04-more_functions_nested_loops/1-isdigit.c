87b221dcd5276ef1d76235a197e59c775df5c11d
DAlons <alonso.morey.arq@gmail.com>
2021-06-25 00:30:56 -0700

    Busqueda de digitos del 0 al 9

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+
+/**
+ * _isdigit - Nos permite buscar numeros
+ * @c: Aqui se almacena el valor a evaluar
+ * Return: Devuelve 1 si es un digito
+ */
+
+int _isdigit(int c)
+{
+return (c >= '0' && c <= '9');
+}
