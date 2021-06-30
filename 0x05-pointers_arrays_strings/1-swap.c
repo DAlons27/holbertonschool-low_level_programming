ebc703b00a4d58423dfc80f18bfab1a898e2fe07
DAlons <alonso.morey.arq@gmail.com>
2021-06-29 18:10:50 -0700

    Intercambio de valores de dos variables

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+ * swap_int - intercambio de numeros
+ * @a: primera variable
+ * @b: segunda variable
+ */
+
+void swap_int(int *a, int *b)
+{
+	*a = 42;
+	*b = 98;
+}
