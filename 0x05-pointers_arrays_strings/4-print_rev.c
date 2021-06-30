77b4bbba0a29a97180d0cdd15c4192c1ddd28e6c
DAlons <alonso.morey.arq@gmail.com>
2021-06-29 21:08:50 -0700

    Cadena invertida

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+
+/**
+ * print_rev - imprime la cadena en reversa
+ * @s: cadena a imprimir
+ */
+void print_rev(char *s)
+{
+	int i, j, len;
+
+	i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+
+	len = i;
+
+	for (j = len - 1; j >= 0; j--)
+	{
+		_putchar(s[j]);
+	}
+
+	_putchar('\n');
+}
