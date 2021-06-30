675f004f873121e82583a274837e88eb14112a3b
DAlons <alonso.morey.arq@gmail.com>
2021-06-29 21:44:59 -0700

    Impresion de cadena en reversa v2

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -4,25 +4,24 @@
  * rev_string - reversa de la cadena
  * @s: cadena
  */
+
 void rev_string(char *s)
 {
-	char c;
-	int i, a, b;
+char c;
+int i, a, b;
 
-	a = 0;
-	b = 0;
+a = 0;
+b = 0;
 
-	while (s[a] != '\0')
-	{
-		a++;
-	}
-
-	b = a - 1;
-
-	for (i = 0; i < a / 2; i++)
-	{
-		c = s[i];
-		s[i] = s[b];
-		s[b--] = c;
-	}
+while (s[a] != '\0')
+{
+a++;
+}
+b = a - 1;
+for (i = 0; i < a / 2; i++)
+{
+c = s[i];
+s[i] = s[b];
+s[b--] = c;
+}
 }
