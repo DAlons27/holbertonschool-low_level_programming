d91720b0094c5a842e5a32c56b38e21da86769a5
DAlons <alonso.morey.arq@gmail.com>
2021-06-30 21:18:10 -0700

    Entero convertido a cadena v2

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,26 +1,26 @@
 #include "holberton.h"
 
 /**
- * _atoi - converts a string to an integer
- * @s: string to be converted
+ * _atoi - convierte la cadena a caracteres
+ * @s: cadena a convertir
  *
- * Return: the int converted from the string
+ * Return: el entero convertido a cadena
  */
 int _atoi(char *s)
 {
-	int i, d, n, len, f, digit;
+	int i, d, n, a, f, digit;
 
 	i = 0;
 	d = 0;
 	n = 0;
-	len = 0;
+	a = 0;
 	f = 0;
 	digit = 0;
 
 	while (s[len] != '\0')
-		len++;
+		a++;
 
-	while (i < len && f == 0)
+	while (i < a && f == 0)
 	{
 		if (s[i] == '-')
 			++d;
