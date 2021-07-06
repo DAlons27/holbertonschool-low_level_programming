76d2109ef81eeb8bed78775e0267de636d931d5c
DAlons <alonso.morey.arq@gmail.com>
2021-07-06 14:36:01 -0700

    función que busque en una cadena cualquiera de un conjunto de bytes

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * *_strpbrk - busca en una cadena cualquiera de un conjunto de bytes
+ * @s: cadena a buscar
+ * @accept: cadena que contiene los bytes a buscar
+ *
+ * Return: puntero al byte
+ */
+char *_strpbrk(char *s, char *accept)
+{
+int i, j;
+
+for (i = 0; *s != '\0'; i++)
+{
+for (j = 0; accept[j] != '\0'; j++)
+{
+if (*s == accept[j])
+{
+return (s);
+}
+}
+s++;
+}
+
+return (NULL);
+}
