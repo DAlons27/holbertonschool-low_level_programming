0085cc4657046b86014cae4871fbba9c38a06fa9
DAlons <alonso.morey.arq@gmail.com>
2021-07-06 17:55:59 -0700

    función que establezca el valor de un puntero en un carácter

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+
+/**
+ * set_string - establece el valor de un puntero en un carácter
+ * @s: puntero de puntero
+ * @to: cadena a colocar
+ */
+void set_string(char **s, char *to)
+{
+*s = to;
+}
