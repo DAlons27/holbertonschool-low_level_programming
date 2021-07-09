63dd361ea9371421f8e5680f64b8627c6744d27b
DAlons <alonso.morey.arq@gmail.com>
2021-07-09 13:56:12 -0700

    programa que imprima su nombre, seguido de una nueva línea

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "holberton.h"
+
+/**
+ * main - imprimir el nombre del programa
+ * @argc: numero de arguementos
+ * @argv: array de argumentos
+ *
+ * Return: Always 0 (Success)
+ */
+int main(int argc __attribute__((unused)), char *argv[])
+{
+printf("%s\n", *argv);
+
+return (0);
+}
