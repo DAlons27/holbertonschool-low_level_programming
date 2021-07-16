8c6920e994655f8a6479a41141b459c8400c26f1
DAlons <alonso.morey.arq@gmail.com>
2021-07-16 16:49:57 -0700

    función que cree una matriz de caracteres y la inicialice con un carácter específico

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include "holberton.h"
+
+/**
+ * *malloc_checked - asigna memoria usando malloc
+ * @b: número de bytes para asignar
+ *
+ * Return: un puntero a la memoria asignada
+ */
+
+void *malloc_checked(unsigned int b)
+{
+void *ptr;
+ptr = malloc(b);
+
+if (ptr == NULL)
+exit(98);
+
+return (ptr);
+}
