2f565507f4b6722d9c40ef7dc66b7ff6891fe2cc
DAlons <alonso.morey.arq@gmail.com>
2021-07-19 10:21:08 -0700

    Macro __FILE__

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
 
+/**
+ *main - imprime el nombre del file al compilar
+ *
+ *Return: retorna 0 si coincide, 1 de lo contrario
+ */
+
 int main(void)
 {
 printf("%s\n", __FILE__);
