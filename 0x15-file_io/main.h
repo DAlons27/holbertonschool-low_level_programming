d45165a337603fd67a9422f049f250a2e8b49f58
DAlons <alonso.morey.arq@gmail.com>
2021-08-09 14:59:12 -0700

    Corrigiendo errorrs

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,5 +1,5 @@
-#ifndef io_c
-#define io_c
+#ifndef IO_C
+#define IO_C
 
 #include <stdlib.h>
 #include <stdio.h>
@@ -8,8 +8,8 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 
-ssize_t read_textfile(const char *filename, size_t letters)
-int create_file(const char *filename, char *text_content)
-int append_text_to_file(const char *filename, char *text_content)
+ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
+int append_text_to_file(const char *filename, char *text_content);
 
 #endif
