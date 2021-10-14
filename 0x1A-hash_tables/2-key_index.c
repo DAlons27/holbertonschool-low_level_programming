aeb3593024235f113c87171287c81f69a518932d
DAlons <alonso.morey.arq@gmail.com>
2021-10-13 22:55:30 -0500

    ash function implementing the djb2 algorithm. hash function implementing the djb2 algori clear ecit exit
    sadsadsa
    
    ,;
    
    git commit -m

---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,23 @@
+#include "hash_tables.h"
+/**
+ * key_index - function tthat gives you the index of a key
+ * @key: string to pass through djb2 algorithm
+ * @size: size of the hash table
+ * Return: the index off the element otherwhise 0
+ */
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	unsigned long int index = 0;
+	unsigned long int number = 0;
+
+	if (key != NULL || size != 0)
+	{
+		number = hash_djb2(key);
+
+		index = number % size;
+
+		return (index);
+	}
+
+	return (index);
+}
